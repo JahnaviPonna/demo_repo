6f44ff7c03f9a187541208de8a0dcab45103867f
JahnaviPonna <jahnavispponna@gmail.com>
2024-07-03 07:06:40 +0000

    name new changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Jahnavi Sushma Priya Ponna",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "jahnavispponna.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
